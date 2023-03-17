d4e72f50fcded628e4130eb9a8b80b697ec859fb
Leekyeongmi <starry3ones@gmail.com>
2023-03-17 19:09:05 +0900

    230317

--- algorithms/효율성/연속 부분수열2.js
+++ algorithms/효율성/연속 부분수열2.js
@@ -11,14 +11,25 @@ function mySolution(M, arr) {
   for (let rightPointer = 0; rightPointer < arr.length; rightPointer++) {
     // rightPointer가 가리키는 값을 더하고 올려나간다.
     sum += arr[rightPointer++];
-    if (sum <= M) counter++;
+    if (sum <= M) {
+      console.log(sum);
+      counter++;
+    }
 
     while (sum >= M) {
       sum -= arr[leftPointer++];
-      if (sum <= M) counter++;
+
+      if (sum <= M) {
+        console.log(sum);
+        counter++;
+      }
     }
   }
   return counter;
 }
 
 console.log(mySolution(5, [1, 3, 1, 2, 3]));
+
+//! 문제
+// 해당 로직으로는 단일원소로 이루어진 부분수열에 대한 합을 구할 수 없어진다. 그렇지 않은가? 아니다.
+// 연속 부분수열1과 무엇이 다른지 차이점을 살펴보면서 각각에 대해 더 깊게 이해해봐야한다.
