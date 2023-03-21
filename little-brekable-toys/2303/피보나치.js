6e24f9ec1cc93f7d41d8144d1e4a2ce83a06f53b
Leekyeongmi <starry3ones@gmail.com>
2023-03-21 10:42:03 +0900

    피보나치 수열

--- little-brekable-toys/2303/피보나치.js
+++ little-brekable-toys/2303/피보나치.js
@@ -1,2 +1,19 @@
 ` TODO: 자연수 n, m이 주어졌을 때, 피보나치 수열의 n번째 수부터 m번째 수까지 합한 값을 구한다. 
 피보나치 수열의 k번째 수는 k - 2번째 수와 k - 1번째 수를 합한 값이다. 수열의 첫 번째 수는 1, 두번째 수는 1이다.`;
+
+function solution(n, m) {
+  let sum = 0;
+  let current = 1;
+  let prev = 0;
+
+  for (let i = 1; i <= m; i++) {
+    if (i >= n) {
+      sum += current;
+    }
+
+    const next = current + prev; // 피보나치 수열의 핵심 개념
+    prev = current;
+    current = next;
+  }
+  return sum;
+}
